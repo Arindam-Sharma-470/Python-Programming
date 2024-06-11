a17f205231d8bb524db48d61b7673e30d2536dd9
Arindam Sharma <arindamsharma470@gmail.com>
2024-06-11 20:02:03 +0530

    First Program

---
+++ First_Program.py
@@ -0,0 +1,14 @@
+# Variable initialization
+a = 10 
+b = 1
+c = a + b
+p = a * b
+def fun(w):
+    print("My name is ", w)
+    li = w.split()
+    print(li)
+fun("Arindam")
+dic = {"city 1": "Mumbai", "city 2":"Hyderabad", "city 3": "Kolkata", "city 4": "Jaipur" }
+print(dic["city 3"])
+tup = (1, 2, 3)
+print( tup )
